ede36758e1d6acf45ad69392dcbcf35924941a9a
praqo <142553687+praqo@users.noreply.github.com>
2023-11-15 21:57:28 -0800

    add useRouter hook to conditionaly render header

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "../styles/styles.scss";
 import Header from "../shared/Header";
 import Footer from "../shared/Footer";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  console.log(router.pathname);
   return (
     <>
-      <Header />
+      {router.pathname !== "/" && <Header />}
       <Component {...pageProps} />
       <Footer />
     </>
